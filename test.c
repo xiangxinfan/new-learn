e28f26b3916df964ec403ddd5fa578d8361e62e9
xiangxinfan <77722815+xiangxinfan@users.noreply.github.com>
2021-07-31 19:35:49 +0800

    Create test.c

---
+++ test.c
@@ -0,0 +1,44 @@
+
+#include <stdio.h>
+#include <string.h>
+
+//int main()
+//{
+//	int a = 10;
+//	int arr[] = { 1,2,3,4,5,6,};    原因在这里：6*4=24
+//	printf("%d\n", sizeof(a));//4个字节     a可以把括号去掉
+//  printf("%d\n", sizeof(int));//4个字节   int不能把括号去掉 
+//  printf("%d\n", sideof(arr));//计算数组大小，单位是字节   是24个字节  因为第8行的原因
+//  printf("%d\n", sideof(arr)/sideof(arr[o]))//总大小除以第一个元素大小是总个数为6个原因还是在第8行，因为是6个数字个数
+//	return 0;
+//}
+//
+//int Max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	int max = 0;
+//	max = Max(num1, num2);
+//	printf("max=%d\n", max);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	if (num1 > num2)
+//		printf("较大值是：%d\n", num1);
+//	else
+//		printf("较大值是：%d\n", num2);
+//
+//	//printf("%d\n", strlen("c:\test\121"));//
+//	return 0;
+//}
